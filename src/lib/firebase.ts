7fd35a2383355596e04cfabacfcd033341da7475
Bishes Guragain <bishesh.guragain@gmail.com>
2025-07-12 09:11:18 +0000

    In the firebase console, in realtime database, after i created the datab

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyB1Att5fmS7zFa_X9-hczg-YhhLAoWimUU",
   authDomain: "timetickr-landing-page.firebaseapp.com",
-  databaseURL: "https://timetickr-landing-page-default-rtdb.firebaseio.com",
+  databaseURL: "https://timetickr-landing-page-default-rtdb.europe-west1.firebasedatabase.app/",
   projectId: "timetickr-landing-page",
   storageBucket: "timetickr-landing-page.appspot.com",
   messagingSenderId: "62667221490",
